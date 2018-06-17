4b197060c12df5e08bf0d8178ccc57d327e81de2
asm <827204212@qq.com>
2018-06-17 19:40:11 +0800

    create

--- src/main/java/com/hc/netty/handler/AppBusinessHandler.java
+++ src/main/java/com/hc/netty/handler/AppBusinessHandler.java
@@ -30,51 +30,51 @@ public class AppBusinessHandler extends ChannelHandlerAdapter {
 		cause.printStackTrace();
 		NettyChannelMap.remove((SocketChannel)ctx.channel());
 		ctx.close();
-		
+
 		HKLogUtils.error(cause);
-		
+
 	}
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg)
 			throws Exception {
-    
-    	 
-         ByteBuf buf =(ByteBuf)msg;
-         byte[] req=new byte[buf.readableBytes()];
-         buf.readBytes(req);
-                 
-         String txcode=new String(req);//交易码         
-                 
-        //1.获取交易码
-         byte[] txcodeArray=new byte[4];
-         System.arraycopy(req,8,txcodeArray,0,4);
-         
-         txcode=new String(txcodeArray,"ascii");
-         System.arraycopy(req,8,txcodeArray,0,4);
-         
-         String reqStr= ISOUtil.hexString(req);
-         LogUtils.info("接收报文>>>>>>txcode="+txcode+";content="+reqStr);
-         
-        
-	    
-        //逻辑分发
-        String actionName="TX"+txcode;
-     	BaseAction baseAction = (BaseAction) SpringApplicationContext.getService(actionName);
-     	
-     	//通过反射，获取该Action的操作方法	
-     	Method method = baseAction.getClass().getMethod("createSendInfo",ChannelHandlerContext.class,Object.class);
-     	method.invoke(baseAction,ctx,req);
-     	
 
-     }
-	
+
+		ByteBuf buf =(ByteBuf)msg;
+		byte[] req=new byte[buf.readableBytes()];
+		buf.readBytes(req);
+
+		String txcode=new String(req);//交易码
+
+		//1.获取交易码
+		byte[] txcodeArray=new byte[4];
+		System.arraycopy(req,8,txcodeArray,0,4);
+
+		txcode=new String(txcodeArray,"ascii");
+		System.arraycopy(req,8,txcodeArray,0,4);
+
+		String reqStr= ISOUtil.hexString(req);
+		LogUtils.info("接收报文>>>>>>txcode="+txcode+";content="+reqStr);
+
+
+
+		//逻辑分发
+		String actionName="TX"+txcode;
+		BaseAction baseAction = (BaseAction) SpringApplicationContext.getService(actionName);
+
+		//通过反射，获取该Action的操作方法
+		Method method = baseAction.getClass().getMethod("createSendInfo",ChannelHandlerContext.class,Object.class);
+		method.invoke(baseAction,ctx,req);
+
+
+	}
+
 
 	@Override
 	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
 		ctx.flush();
 	}
-    
+
 //	@Override
 //	public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
 //		if (IdleStateEvent.class.isAssignableFrom(evt.getClass())) {
@@ -85,14 +85,14 @@ public class AppBusinessHandler extends ChannelHandlerAdapter {
 //				 LogUtils.info("读空闲");
 //				else if (event.state() == IdleState.WRITER_IDLE)
 //					LogUtils.info("写空闲");
-//			 
+//
 //			if (event.state() == IdleState.ALL_IDLE){
 //				 LogUtils.info("此链接已经很长时间没活动了，需要移除;连接信息:"+ctx.channel());
 //				 NettyChannelMap.remove((SocketChannel)ctx.channel());
 //				 SocketChannel sc=(SocketChannel)ctx.channel();
 //				 ctx.close();
 //				 sc.close();
-//				 
+//
 //				 if(sc.isActive()){
 //					 LogUtils.info("是活动的");
 //				 }
@@ -103,5 +103,5 @@ public class AppBusinessHandler extends ChannelHandlerAdapter {
 //			}
 //		}
 //	}
-	
+
 }
